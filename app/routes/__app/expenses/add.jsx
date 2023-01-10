97862c447bab58834af64cabbaf33bb7a49d285d
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-01-10 23:57:09 +0800

    Add server side validation in submitting the form.

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 import { addExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -21,6 +22,12 @@ export default function AddExpensesPage() {
 export async function action({ request, params }) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
 
   await addExpense(expenseData);
   return redirect("/expenses");
