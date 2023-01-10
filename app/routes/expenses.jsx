776b6609163a6ad52827bc01a1a6b60243b00c73
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-01-10 11:13:39 +0800

    refactoring

--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,12 +1,30 @@
 import { Outlet } from "@remix-run/react";
+import ExpensesList from "~/components/expenses/ExpensesList";
 import expensesStyles from "~/styles/expenses.css";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
 
 export default function ExpensesLayout() {
   return (
-    <main>
-      <p>Shared Element!</p>
+    <>
       <Outlet />
-    </main>
+      <main>
+        <ExpensesList expenses={DUMMY_EXPENSES} />
+      </main>
+    </>
   );
 }
 
