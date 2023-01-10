c287fff1ce9db775efd73f431a3f8bfd1d6a13d6
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-01-10 10:58:02 +0800

    add custom fonts.

--- app/root.jsx
+++ app/root.jsx
@@ -19,6 +19,16 @@ export default function App() {
     <html lang="en">
       <head>
         <Meta />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
         <Links />
       </head>
       <body>
