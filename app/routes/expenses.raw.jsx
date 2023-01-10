ca68d87db004fc32455cc5552dbe2aa67810fb5f
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-01-10 21:28:10 +0800

    Implement Resource routes.

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,3 +1,18 @@
-export default function ExpensesRawPage() {
-  return <h1>Expenses Raw Page</h1>;
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export function loader() {
+  return DUMMY_EXPENSES;
 }
