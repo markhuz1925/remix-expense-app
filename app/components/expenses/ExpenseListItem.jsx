b758d3cb86fdbef23b977c9fbc2776e0185f557c
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-01-12 07:50:50 +0800

    refactor deleting selected data using useFetcher()

--- app/components/expenses/ExpenseListItem.jsx
+++ app/components/expenses/ExpenseListItem.jsx
@@ -1,8 +1,18 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useFetcher } from "@remix-run/react";
 
 function ExpenseListItem({ id, title, amount }) {
+  const fetcher = useFetcher();
+
   function deleteExpenseItemHandler() {
-    // tbd
+    fetcher.submit(null, { method: "delete", action: `/expenses/${id}` });
+  }
+
+  if (fetcher.state !== "idle") {
+    return (
+      <article className="expense-item locked">
+        <p>Deleting...</p>
+      </article>
+    );
   }
 
   return (
@@ -12,10 +22,10 @@ function ExpenseListItem({ id, title, amount }) {
         <p className="expense-amount">${amount.toFixed(2)}</p>
       </div>
       <menu className="expense-actions">
-        {/* <button onClick={deleteExpenseItemHandler}>Delete</button> */}
-        <Form method="delete" action={`/expenses/${id}`}>
+        <button onClick={deleteExpenseItemHandler}>Delete</button>
+        {/* <Form method="delete" action={`/expenses/${id}`}>
           <button>Delete</button>
-        </Form>
+        </Form> */}
         <Link to={id}>Edit</Link>
       </menu>
     </article>
