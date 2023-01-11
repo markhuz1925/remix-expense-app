fa1ddfab73ac392ee1fe6026d4401513c6b3b8c1
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-01-12 07:19:03 +0800

    Updating data.

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -38,3 +38,19 @@ export async function getExpense(id) {
     throw error;
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    await prisma.expense.update({
+      where: { id },
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
--- app/routes/__app/expenses/$id.jsx
+++ app/routes/__app/expenses/$id.jsx
@@ -1,6 +1,9 @@
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { updateExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 // import { getExpense } from "~/data/expenses.server";
 
 export default function UpdateExpensesPage() {
@@ -22,3 +25,18 @@ export default function UpdateExpensesPage() {
 //   const expense = await getExpense(expenseId);
 //   return expense;
 // }
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect("/expenses");
+}
